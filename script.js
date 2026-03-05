f882493110329aef1bef2da255c1c7bb1f797136
Ai5678 <99451699+Ai5678@users.noreply.github.com>
2026-03-05 15:27:22 -0700

    Tickers list render

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log(tickerArr);
 
 const tickerInput = document.getElementById("ticker-input")
 const addTickerBtn = document.getElementById("add-ticker-btn")
+const tickersList = document.getElementById("tickers-list")
 
 function addTicker(tickerArr){
     addTickerBtn.addEventListener("click", (e) => {
@@ -10,9 +11,17 @@ function addTicker(tickerArr){
         if (tickerArr.length < 3){
             const ticker = tickerInput.value.toUpperCase();
             tickerArr.push(ticker);
-            console.log(tickerArr);
+            displayTickers(tickerArr);
+        } else{
+            tickersList.innerHTML = `<li class="error">You can only add up to 3 tickers</li>`
         }
+
         tickerInput.value = "";
     })
 }
+
+function displayTickers(tickerArr){
+    tickersList.innerHTML = "Your tickers: " + tickerArr.map(ticker => `<li>${ticker}</li>`).join("");
+}
+
 addTicker(tickerArr);
