03e516fed3da42a8dec873392790451a0b7975c8
Ai5678 <99451699+Ai5678@users.noreply.github.com>
2026-03-05 16:06:31 -0700

    connected to gemini api

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import {GoogleGenAI} from "@google/genai";
 
 const ticker ="MSFT"
 const startDate = "2026-03-01";
@@ -9,3 +10,15 @@ const url = `https://api.massive.com/v2/aggs/ticker/${ticker}/range/1/day/${star
 const response = await fetch(url);
 const data = await response.json();
 console.log(data);
+
+const genai = new GoogleGenAI(process.env.GEMINI_API_KEY);
+
+async function generateReport(){
+    const response = await genai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: "What is the capital of Canada?"
+    })
+
+    console.log(response.text);
+}
+await generateReport();
